9c39dbd84824fb104df7ea474ad68c153e37cb2a
omarsabri666 <101377155+omarsabri666@users.noreply.github.com>
2023-07-12 19:22:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log("Hello world!");
+
+const myName = "Jonas Schmedtmann";
+const h1 = document.querySelector(".heading-primary");
+console.log(myName);
+console.log(h1);
+
+// h1.addEventListener("click", function () {
+//   h1.textContent = myName;
+//   h1.style.backgroundColor = "red";
+//   h1.style.padding = "5rem";
+// });
+
+///////////////////////////////////////////////////////////
+// Set current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.add("nav-open");
+});
+
+
+
+
+var element = document.querySelector("#current-0")
+if (element) {
+    element.textContent = dice
+}
